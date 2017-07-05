5c31da0a6cd4dabec05f286197984b66874ab5ad
Kamil Kuzminski <kamil.kuzminski@codefog.pl>
2017-07-05 14:24:36 +0200

    Updated the author note

--- src/Derhaeuptling/MobileContent/Upgrade.php
+++ src/Derhaeuptling/MobileContent/Upgrade.php
@@ -4,6 +4,8 @@
  * mobilecontent extension for Contao Open Source CMS
  *
  * @author  Kamil Kuzminski <https://github.com/qzminski>
+ * @author  derhaeuptling <https://derhaeuptling.com>
+ * @author  Martin Schwenzer <mail@derhaeuptling.com>
  * @license LGPL
  */
 
