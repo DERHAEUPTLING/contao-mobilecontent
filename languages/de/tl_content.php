70a5a6902c03c4f88724cf45a4288e3f316d75ad
Martin Schwenzer <mail@derhaeuptling.com>
2017-08-22 10:47:51 +0200

    update languages

--- languages/de/tl_content.php
+++ languages/de/tl_content.php
@@ -14,4 +14,5 @@
  */
 $GLOBALS['TL_LANG']['tl_content']['mobileImage'] = ['Anderes Bild für Mobilgeräte', 'Hier können Sie ein anderes Bild für Mobilgeräte verwenden.'];
 $GLOBALS['TL_LANG']['tl_content']['mobileImageSrc'] = ['Quelldatei des mobilen Bildes', 'Bitte wählen Sie die Datei des mobilen Bildes.'];
-$GLOBALS['TL_LANG']['tl_content']['mobileImageSize'] = ['Bildgröße des mobilen Bildes', 'Hier können Sie die Größe des mobilen Bildes festlegen. Here you can enter a mobile image size. If none provided, the size will be inherited from main image.'];
+$GLOBALS['TL_LANG']['tl_content']['mobileImageCustomSize'] = ['Andere Bildgröße', 'Hier können Sie eine andere Bildgröße für das mobile Bild festlegen. Wenn nicht ausgewählt, dann wird die Bildgrößeneinstellung des normalen Bildes verwendet.'];
+$GLOBALS['TL_LANG']['tl_content']['mobileImageSize'] = ['Bildgröße des mobilen Bildes', 'Hier können Sie die Größe des mobilen Bildes festlegen.'];
